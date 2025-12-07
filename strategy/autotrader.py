7e334bdf42731ccedac6005c3b99188006d17943
Ayush <ayushgundawar@gmail.com>
2025-12-07 08:59:12 -0800

    more layouts

--- strategy/autotrader.py
+++ strategy/autotrader.py
@@ -3,7 +3,11 @@ class Strategy:
         pass
 
     def on_new_post(self, tweet):
+        # Trigger grok research. Provide the last 10 unique tweets and their timestamps, as well as the state of the book.
+        # Ask for a trading decision. If "yes", send a buy order for yes. If "no" send a buy order for no. Both should be IOC.
         pass
 
     def on_new_book(self, book):
-        pass
+        # Update the local book snapshot for this market
+        # Store the book as a class variable
+        print(book)
